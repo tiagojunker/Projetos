1901fe75767f8e60b2e4ff76da541e636a8537b9
Tiago Junker <92689798+tiagojunker@users.noreply.github.com>
2022-05-11 10:19:35 -0300

    Mudança Js

--- pizza/script.js
+++ pizza/script.js
@@ -5,6 +5,9 @@ let queijos4 = document.getElementById('4');
 let pepperoni = document.getElementById('5');
 let mangueira = document.getElementById('6');
 let qtdpedido = document.getElementById('qtd');
+let carrinho = document.getElementById('carrinho');
+let navbar = document.getElementsByTagName('nav')[0];
+let ativar = 0
 
 let adicionadas = []
 
@@ -22,11 +25,38 @@ function adicionarPizza(elemento) { //Adiciona pedido a lista
 function mudarCarrinho() {     //Mostra quantidade de produtos no carrinho
     if (adicionadas.length != 0) {
         if(adicionadas.length <= 5) {
-            qtdpedido.style.display = 'flex'
-            qtdpedido.innerHTML = `${adicionadas.length}`  
+            qtdpedido.style.display = 'flex';
+            qtdpedido.innerHTML = `${adicionadas.length}` ; 
         } else {
-            qtdpedido.style.display = 'flex'
-            qtdpedido.innerHTML = `5+`
+            qtdpedido.style.display = 'flex';
+            qtdpedido.innerHTML = `5+`;
         }
     }
 }
+
+function criarCarrinho() {
+    if(ativar == 0) {
+        ativar ++
+        let container = document.createElement('div');
+        container.classList.toggle('container-carrinho');
+        qtdpedido.appendChild(container);
+    }
+
+    for(let i of adicionadas) {
+        let sabor = document.createElement('p');
+        container.appendChild(sabor);
+        sabor.innerHTML = `${i}`;
+    }
+}
+
+function fecharCarrinho() {
+    let containerC = document.getElementsByClassName('.container-carrinho');
+    containerC.style.display = 'none'
+}
+
+if(!criarCarrinho) {
+    carrinho.addEventListener('click', criarCarrinho)
+} else {
+    carrinho.addEventListener('click', fecharCarrinho)
+}
+
