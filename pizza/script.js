66a080c1b41e9580fdc598a2202793b8cec82415
Tiago Junker <92689798+tiagojunker@users.noreply.github.com>
2022-05-16 12:18:01 -0300

    Resolvi Bug to do list

--- pizza/script.js
+++ pizza/script.js
@@ -9,7 +9,7 @@ let carrinho = document.getElementById('carrinho');
 let navbar = document.getElementsByTagName('nav')[0];
 let ativar = 0;
 
-let adicionadas = []
+let adicionadas = [] //Array com os pedidos adicionados
 
 function identificarId(elemento) { //identifica ID de um Elemento
     let identidade = elemento.id;
@@ -22,7 +22,7 @@ function adicionarPizza(elemento) { //Adiciona pedido a lista
     mudarCarrinho();
 }
 
-function mudarCarrinho() {     //Mostra quantidade de produtos no carrinho
+function mudarCarrinho() {     //Altera quantidade de produtos no carrinho
     if (adicionadas.length != 0) {
         if(adicionadas.length <= 5) {
             qtdpedido.style.display = 'flex';
@@ -31,19 +31,24 @@ function mudarCarrinho() {     //Mostra quantidade de produtos no carrinho
             qtdpedido.style.display = 'flex';
             qtdpedido.innerHTML = `5+`;
         }
+    } else {
+        qtdpedido.style.display = 'none';
+        qtdpedido.innerHTML = `${adicionadas.length}` ;
     }
 }
 
+//função que cria a janela de pedidos
 function criarCarrinho() {
     let container = document.createElement('div');
 
-    if(ativar == 0) {
+    if(ativar == 0) { //se ativer igual a zero abre a janela quando a função é executada
         ativar++
         container.classList.add('container-carrinho');
         container.id = 'container-carrinho';
         qtdpedido.appendChild(container);
 
-        for(let i of adicionadas) {
+        // função que cria os elementos adicionados
+        for(let i of adicionadas) { 
             let containerex = document.createElement('div');
             let sabor = document.createElement('p');
             let x = document.createElement('p');
@@ -51,6 +56,7 @@ function criarCarrinho() {
     
             x.id = `${i}del`;
             containerex.id = `${i}c`;
+            //função que remove os elementos adicionados
             x.onclick = function () {
                 let recuperarId = this.id;
 
@@ -64,7 +70,7 @@ function criarCarrinho() {
                 
                 container.removeChild((document.getElementById(`${recuperarId}c`)));
                 //filter ou slice;
-
+                adicionadas.splice(adicionadas.indexOf(recuperarId),1);
             }
             containerex.style.display = 'flex';
             containerex.style.justifyContent = 'space-between';
@@ -76,17 +82,22 @@ function criarCarrinho() {
 
             x.innerHTML = 'X';
             sabor.innerHTML = `${i}`;
-
         }
 
-    } else {
+    } else { //se ativar igual a um fecha a janela quando a função é executada
         ativar--;
         let container = document.getElementById('container-carrinho');
         qtdpedido.removeChild(container);
+        mudarCarrinho();
     }
 }
 
-
+//função que verifica caso todos os pedidos adicionados sejam retirados
+setInterval(function carrinhoMenor(){
+    if(adicionadas.length == 0){
+        mudarCarrinho();
+    }
+}, 500)
 carrinho.addEventListener('click', criarCarrinho);
 
 
